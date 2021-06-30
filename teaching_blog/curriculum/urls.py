d5d0076da237ddd5375cb4c304a8ffd766ed3f31
rikki1203 <rikhilaannem@gmail.com>
2021-06-30 18:12:33 +0530

    created a class-based view and updated the urls and designed a new template, will resume tomorrow

---
+++ teaching_blog/curriculum/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name='curriculum'
+
+urlpatterns = [
+    path('',views.StandardListView.as_view(),name='standard_list')
+]
--- teaching_blog/teaching_blog/urls.py
+++ teaching_blog/teaching_blog/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 import app_users
 from django.contrib import admin
 from django.urls import path,include
-
+import curriculum
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app_users.urls'))
+    path('',include('app_users.urls')),
+    path('curriculum/',include('curriculum.urls'))
+
 ]
 
 from django.conf import settings
@@ -28,4 +30,3 @@ from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    
